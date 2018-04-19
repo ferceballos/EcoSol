babe63e8e37dce0001428938d4495d2d1a84bcce
Fer Ceballos <unceballos@gmail.com>
2018-04-19 12:18:48 -0500

    Added products section

--- js/initializations.js
+++ js/initializations.js
@@ -9,3 +9,21 @@ $('#collapsibleTwo').collapsible();
 
 var elem = document.querySelector('.collapsibleTwo');
 var instance = M.Collapsible.init(elem, {});
+
+var elem = document.querySelector('.panel1');
+var instance = M.Materialbox.init(elem, {});
+
+var elem = document.querySelector('.panel2');
+var instance = M.Materialbox.init(elem, {});
+
+var elem = document.querySelector('.panel3');
+var instance = M.Materialbox.init(elem, {});
+
+var elem = document.querySelector('.panel4');
+var instance = M.Materialbox.init(elem, {});
+
+var elem = document.querySelector('.panel5');
+var instance = M.Materialbox.init(elem, {});
+
+var elem = document.querySelector('.dropdown-trigger');
+var instance = M.Dropdown.init(elem, {});
