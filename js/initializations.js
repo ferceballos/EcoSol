18118802f853994cd647abc1311236434b8e69e9
Fer Ceballos <unceballos@gmail.com>
2018-05-20 01:46:41 -0500

    Added scrollspy to handle animations on scroll and a brand section has been added

--- js/initializations.js
+++ js/initializations.js
@@ -27,3 +27,45 @@ var instance = M.Materialbox.init(elem, {});
 
 var elem = document.querySelector('.dropdown-trigger');
 var instance = M.Dropdown.init(elem, { alignment: 'left', constrainWidth: false, coverTrigger: false, hover: true });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.scrollspy');
+    var instances = M.ScrollSpy.init(elems, {
+        getActiveElement: function (id) {
+            if (id == "contact") {
+                console.log(id);
+                var whats = anime({
+                    targets: '#whats input',
+                    value: 3121660489,
+                    round: 1,
+                    easing: 'easeInOutExpo',
+                    duration: 2000
+                });
+
+                var phone = anime({
+                    targets: '#phone input',
+                    value: 3123254888,
+                    round: 1,
+                    easing: 'easeInOutExpo',
+                    duration: 2500
+                });
+
+                var phone = anime({
+                    targets: '#face input',
+                    value: "EcoSol",
+                    round: 1,
+                    easing: 'easeInOutExpo',
+                    duration: 2500
+                });
+
+            }
+
+            else {
+                console.log(id)
+            }
+            return 'a[href="#' + id + '"]';
+        }
+    });
+});
+
+
