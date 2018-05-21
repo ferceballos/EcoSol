4ecc44dc13f2222dfceacac2acb415520e1d814b
Fer Ceballos <fde_jesus0@ucol.mx>
2018-05-21 17:36:47 -0500

    Animations of 'brand logos' and contact' has been completed

--- js/animations.js
+++ js/animations.js
@@ -1,9 +1,76 @@
-var lineDrawing = anime({
-    targets: '#lineDrawing .lines path',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'easeInOutSine',
-    duration: 1500,
-    delay: function (el, i) { return i * 250 },
-    direction: 'alternate',
-    loop: true
+$(document).ready(function () {
+    /* Flags and other setup varaibles */
+    var contact = false;
+    var brands = false;
+    $('.logo .el').hide();
+
+    function checkVisible(elm, evalType) {
+        evalType = evalType || "visible";
+
+        var vpH = $(window).height(), // Viewport Height
+            st = $(window).scrollTop(), // Scroll Top
+            y = $(elm).offset().top,
+            elementHeight = $(elm).height();
+
+        if (evalType === "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
+        if (evalType === "above") return ((y < (vpH + st)));
+    }
+
+    var lineDrawing = anime({
+        targets: '#lineDrawing .lines path',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutSine',
+        duration: 1500,
+        delay: function (el, i) { return i * 250 },
+        direction: 'alternate',
+        loop: true
+    });
+
+    $(document).ready(function () {
+        $('.scrollspy').scrollSpy(
+            {
+                getActiveElement: function (id) {
+                    if (id == "contact" && !contact) {
+                        console.log(id);
+
+                        var contactInfo = anime({
+                            targets: '.fadedIn .el',
+                            translateX: [200, 0],
+                            opacity: [0, 1],
+                            easing: 'easeInOutQuad',
+                            delay: function (el, i) { return 400 + (i * 140); },
+                        });
+
+                        var map = anime({
+                            targets: '.map',
+                            translateY: [200, 0],
+                            opacity: [0, 1],
+                            easing: 'easeInOutQuad',
+                            delay: 300
+                        });
+
+
+                        contact = true;
+                    }
+
+                    else if (checkVisible($("#marcas"), "visible") && !brands) {
+                        $('.logo .el').show();
+
+                        var logos = anime({
+                            targets: '.logosanime .logo .el',
+                            translateY: [100, 0],
+                            opacity: [0, 1],
+                            easing: 'easeInOutExpo',
+                            delay: function (el, i) { return 100 + (i * 140); },
+                        });
+
+                        brands = true;
+                    }
+                }
+            }
+        );
+    });
+
 });
+
+
--- js/initializations.js
+++ js/initializations.js
@@ -1,62 +1,37 @@
-var elem = document.querySelector('.slider');
-var instance = M.Slider.init(elem, { indicators: false, interval: 7000 });
+$(document).ready(
+    function () {
+        var elem = document.querySelector('.slider');
+        var instance = M.Slider.init(elem, { indicators: false, interval: 7000 });
 
-var elem = document.querySelector('.collapsible');
-var instance = M.Collapsible.init(elem, {});
+        var elem = document.querySelector('.collapsible');
+        var instance = M.Collapsible.init(elem, {});
 
+        $('#collapsibleTwo').collapsible();
 
-$('#collapsibleTwo').collapsible();
+        var elem = document.querySelector('.collapsibleTwo');
+        var instance = M.Collapsible.init(elem, {});
 
-var elem = document.querySelector('.collapsibleTwo');
-var instance = M.Collapsible.init(elem, {});
+        var elem = document.querySelector('.panel1');
+        var instance = M.Materialbox.init(elem, {});
 
-var elem = document.querySelector('.panel1');
-var instance = M.Materialbox.init(elem, {});
+        var elem = document.querySelector('.panel2');
+        var instance = M.Materialbox.init(elem, {});
 
-var elem = document.querySelector('.panel2');
-var instance = M.Materialbox.init(elem, {});
+        var elem = document.querySelector('.panel3');
+        var instance = M.Materialbox.init(elem, {});
 
-var elem = document.querySelector('.panel3');
-var instance = M.Materialbox.init(elem, {});
+        var elem = document.querySelector('.panel4');
+        var instance = M.Materialbox.init(elem, {});
 
-var elem = document.querySelector('.panel4');
-var instance = M.Materialbox.init(elem, {});
+        var elem = document.querySelector('.panel5');
+        var instance = M.Materialbox.init(elem, {});
 
-var elem = document.querySelector('.panel5');
-var instance = M.Materialbox.init(elem, {});
+        var elem = document.querySelector('.dropdown-trigger');
+        var instance = M.Dropdown.init(elem, { alignment: 'left', constrainWidth: false, coverTrigger: false, hover: true });
 
-var elem = document.querySelector('.dropdown-trigger');
-var instance = M.Dropdown.init(elem, { alignment: 'left', constrainWidth: false, coverTrigger: false, hover: true });
+        $("#whats").hover(function handlerIn() {
 
-$("#whats").hover(function handlerIn() {
-
-}, function handlerOut() { });
-
-var seen = false;
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.scrollspy');
-    var instances = M.ScrollSpy.init(elems, {
-        getActiveElement: function (id) {
-            if (id == "contact" && !seen) {
-                console.log(id);
-
-                var whats = anime({
-                    targets: '#whats',
-                    translateX: [200, 0],
-                    opacity: [0, 1],
-                    easing: 'easeInOutQuad',
-                    delay: 400
-                });
-
-                seen = true;
-            }
-
-            else {
-                console.log(id)
-            }
-            return 'a[href="#' + id + '"]';
-        }
-    });
-});
-
+        }, function handlerOut() { });
+    }
+)
 
