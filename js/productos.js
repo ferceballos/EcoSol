3ee124aebb7c64847a5e43b96a029fdf9786c788
Fer Ceballos <unceballos@gmail.com>
2018-06-17 00:05:54 -0500

    Finally made an animation for products.html that i like

--- js/productos.js
+++ js/productos.js
@@ -1,98 +1,75 @@
 $(document).ready(() => {
 
-    /* Flags and other setup varaibles */
-    var products = false;
-    var InversorAnimation = anime.timeline();
+    var elem = document.querySelector('.dropdown-trigger');
+    var instance = M.Dropdown.init(elem, { alignment: 'left', constrainWidth: false, coverTrigger: false, hover: true, autoTrigger: false });
 
-    InversorAnimation
-        .add({
-            targets: '#inversorpan',
-            backgroundColor: '#AFF8DB',
-            easing: 'easeInOutQuart',
-            translateY: [-100, 0],
-            elasticity: 200,
-            duration: 300
-        })
-        .add({
-            targets: '#inverButton',
-            rotate: {
-                value: 180,
-                duration: 100,
-                easing: 'easeInOutSine'
-            }
-        })
-        ;
+    let duration = 180;
+    let radius = ['0em', '10em'];
 
-    InversorAnimation.pause();
-    InversorAnimation.reverse();
-
-
-    $("#products-wrapper").invisible();
-
-    /* Funtion to check if an element is on screen */
-    function checkVisible(elm, evalType) {
-        evalType = evalType || "visible";
-
-        var vpH = $(window).height(), // Viewport Height
-            st = $(window).scrollTop(), // Scroll Top
-            y = $(elm).offset().top,
-            elementHeight = $(elm).height();
-
-        if (evalType === "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
-        if (evalType === "above") return ((y < (vpH + st)));
-    }
-
-    function checkWindow() {
-        if (checkVisible($("#products-wrapper"), "visible") && !products) {
-            $("#products-wrapper").visible();
-
-            var letters = anime({
-                targets: '.productsheader',
-                translateY: [100, 0],
-                opacity: [0, 1],
-                easing: 'easeInOutExpo',
-                delay: function (productsheader, i) { return 200 + (i * 200); },
-            });
-
-
-            var productsAnimation = anime({
-                targets: '.products',
-                translateY: [60, 0],
-                opacity: [0, 1],
-                easing: 'easeInOutExpo',
-                delay: function (products, i) { return 800 + (i * 200); },
-            });
-
-            products = true;
-        }
-    }
-
-    checkWindow();
-
-
-
-    $(window).scroll(() => {
-        checkWindow();
+    /* Animations */
+    var panel = anime({
+        targets: '.panelimg',
+        opacity: .8,
+        borderRadius: radius,
+        easing: 'easeInOutQuad',
+        scale: 0.6,
+        duration: duration,
     });
 
-    $("#inversors").mouseenter(() => {
-        console.log("enter");
-        InversorAnimation.play();
-        InversorAnimation.reverse();
-    })
+    var calentador = anime({
+        targets: '.calentadorimg',
+        opacity: .8,
+        borderRadius: radius,
+        easing: 'easeInOutQuad',
+        scale: 0.6,
+        duration: duration,
+    });
 
-    $("#inversors").mouseleave(() => {
-        console.log("leave");
-        InversorAnimation.play();
-        InversorAnimation.reverse();
+    var inversor = anime({
+        targets: '.inversorimg',
+        opacity: .8,
+        borderRadius: radius,
+        easing: 'easeInOutQuad',
+        scale: 0.6,
+        duration: duration,
+    });
 
+    /*
+    panel.pause();
+    panel.reverse(); 
+    calentador.pause();
+    calentador.reverse(); 
+    inversor.pause();
+    inversor.reverse(); 
+    */
 
-    })
+    $("#panel").mouseenter(() => {
+        panel.play();
+        panel.reverse();
+    });
 
+    $("#panel").mouseleave(() => {
+        panel.play();
+        panel.reverse();
+    });
 
-    $(".ps").mouseenter((e) => {
-        console.log(e.clientX, e.clientY)
-    })
+    $("#calentador").mouseenter(() => {
+        calentador.play();
+        calentador.reverse();
+    });
 
-})
+    $("#calentador").mouseleave(() => {
+        calentador.play();
+        calentador.reverse();
+    });
 
+    $("#inversor").mouseenter(() => {
+        inversor.play();
+        inversor.reverse();
+    });
+
+    $("#inversor").mouseleave(() => {
+        inversor.play();
+        inversor.reverse();
+    });
+});
